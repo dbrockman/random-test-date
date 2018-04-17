3999a5e19336cfd66175783ec0d6879e9a638d1d
David Brockman Smoliansky <david@springworks.se>
2018-04-17 12:30:28 +0200

    code

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+const ms = (v: Date | number | string): number =>
+  typeof v === "number"
+    ? v
+    : (typeof v === "string" ? new Date(v) : v).getTime();
+
+const min = ms("0000-01-01T00:00:00.000Z");
+const max = ms("9999-12-31T23:59:59.999Z");
+
+/**
+ * @returns a new date with a random time between `0000-01-01T00:00:00.000Z` and `9999-12-31T23:59:59.999Z`.
+ */
+export const randomDate = () =>
+  new Date(Math.floor(Math.random() * (max - min + 1)) + min);
+
+/**
+ * Add the specified number of milliseconds to the given date.
+ * @param date the reference date
+ * @param amount the amount of milliseconds to be added
+ * @returns the new date with the milliseconds added to the reference date
+ */
+export const addMilliseconds = (date: Date | number | string, amount: number) =>
+  new Date(ms(date) + amount);
+
+/**
+ *
+ * @param date the reference date
+ * @returns the new date with one millisecond added to the reference date
+ */
+export const nextDate = (date: Date | number | string) =>
+  addMilliseconds(date, 1);
---
+++ test/test.ts
@@ -0,0 +1,28 @@
+import { addMilliseconds, nextDate, randomDate } from "../src";
+
+test("that randomDate return a valid date", () => {
+  expect(randomDate()).toBeInstanceOf(Date);
+  expect(randomDate().getTime()).not.toBeNaN();
+});
+
+test("that addMilliseconds return a new date", () => {
+  const d1 = randomDate();
+  const d2 = addMilliseconds(d1, 0);
+  expect(d1 === d2).toBe(false);
+  expect(d1.getTime() === d2.getTime()).toBe(true);
+  expect(randomDate().getTime() === randomDate().getTime()).toBe(false);
+});
+
+test("that addMilliseconds adds the amout of milliseconds", () => {
+  const ref = randomDate().getTime();
+  expect(addMilliseconds(ref, 0).getTime()).toBe(ref);
+  expect(addMilliseconds(ref, 1).getTime()).toBe(ref + 1);
+  expect(addMilliseconds(ref, 3).getTime()).toBe(ref + 3);
+  expect(addMilliseconds(ref, -1).getTime()).toBe(ref - 1);
+  expect(addMilliseconds(ref, -3).getTime()).toBe(ref - 3);
+});
+
+test("that nextDate adds one millisecond", () => {
+  const ref = randomDate().getTime();
+  expect(nextDate(ref).getTime()).toBe(ref + 1);
+});
